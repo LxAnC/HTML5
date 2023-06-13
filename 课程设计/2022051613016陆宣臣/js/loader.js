f7b874eb9d6daa82ef4d04921ba850df8640c37a
LuXuAn <5001363@qq.com>
2023-06-13 19:10:55 +0800

    加入了码云大佬的特效存档

---
+++ 课程设计/2022051613016陆宣臣/js/loader.js
@@ -0,0 +1,28 @@
+let list = document.querySelectorAll('.li')
+
+        function setActiveClass() {
+            list.forEach((item) =>
+                item.classList.remove('act'));
+            this.classList.add('act');
+            
+        }
+        
+
+        function setNoClass(){
+           
+            this.classList.remove('act');
+            list[0].classList.add('act');
+            
+        }
+        window.onload = function Home(){
+            
+            list.forEach(function(item){ 
+            item.addEventListener('mouseover', setActiveClass);
+            item.addEventListener('mouseleave',setNoClass);
+            })
+            
+            
+        }
+       let nav=document.querySelector('na')
+        
+       
